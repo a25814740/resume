9387793e9ad7c7e20714d230bc19a3acd5e9091a
a25814740 <a25814740@gmail.com>
2026-07-12 10:15:48 +0800

    chore: remove old versions from v5 branch

--- v3/src/main.ts
+++
@@ -1,8 +0,0 @@
-import { createApp } from 'vue';
-import App from './App.vue';
-import './styles/tokens.css';
-import './styles/base.css';
-import './styles/components.css';
-import './styles/motion.css';
-
-createApp(App).mount('#app');
